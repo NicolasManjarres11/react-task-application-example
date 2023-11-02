ecd82a4f4a8b56dff01b16824f59987ed4915279
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2023-11-01 20:36:17 -0500

    react task aplication

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,47 @@
+import { useState, useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+function TaskForm() {
+  const [title, settitle] = useState("");
+  const [description, setdescription] = useState("");
+  const { createTask } = useContext(TaskContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, description);
+    createTask({
+      title,
+      description,
+    });
+    settitle("");
+    setdescription("");
+  };
+
+  return (
+    <div className="max-w-md mx-auto">
+      <form className="bg-slate-800 p-10 mb-4" onSubmit={handleSubmit}>
+        <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
+        <input
+          placeholder="Escribe tu tarea"
+          onChange={(e) => {
+            settitle(e.target.value);
+          }}
+          value={title}
+          className="bg-slate-300 p-3 w-full mb-2"
+          autoFocus
+        />
+        <textarea
+          placeholder="Descripción de la tarea"
+          onChange={(e) => {
+            setdescription(e.target.value);
+          }}
+          value={description}
+          className="bg-slate-300 p-3 w-full mb-2"
+        ></textarea>
+        <button className="bg-indigo-500 px-3 py-1 text-white">Guardar</button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskForm;
---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'mi primer tarea',
+        description: 'mi primer tarea'
+    },
+    {
+        id: 1,
+        title: 'mi segumda tarea',
+        description: 'mi segumda tarea'
+    },
+    {
+        id: 2,
+        title: 'mi tercer tarea',
+        description: 'mi tercer tarea'
+    },
+    {
+        id: 3,
+        title: 'mi cuarta tarea',
+        description: 'mi cuarta tarea'
+    },
+]
